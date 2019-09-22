f5c0961fb53ed2e5306626cf3c192d9473809f16
Thiago Sonoda <thiago.sonoda2012@gmail.com>
2019-09-22 00:44:01 -0300

    Consulta simples com query methods

--- src/main/java/com/tsonoda/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/tsonoda/workshopmongo/repository/PostRepository.java
@@ -1,11 +1,13 @@
 package com.tsonoda.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tsonoda.workshopmongo.domain.Post;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
-
+public interface PostRepository extends MongoRepository<Post, String> {
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
